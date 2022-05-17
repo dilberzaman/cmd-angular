080390a4c4ef1a7502ddb7e3e0a825eb535e07eb
Basavaraj <basavaraj.bhavi@pratian.com>
2022-05-17 19:19:45 +0530

    added team-1

--- src/app/cmd/appointments/component/view-appointment/components/view-all-prescription/view-all-prescription.component.ts
+++ src/app/cmd/appointments/component/view-appointment/components/view-all-prescription/view-all-prescription.component.ts
@@ -1,18 +1,40 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ViewappointmentService } from '../../Services/viewappointment.service';
+// @Component({
+//   selector: 'app-view-all-prescription',
+//   templateUrl: './view-all-prescription.component.html',
+//   styleUrls: ['./view-all-prescription.component.scss']
+// })
+// export class ViewAllPrescriptionComponent implements OnInit {
+//   Medicine: any[]= [];
+//   constructor(private  ViewappointmentService:ViewappointmentService) { }
+
+//   ngOnInit(): void {
+//     this.ViewappointmentService.getmedicine()
+
+//     .subscribe(Medicine=>this.medicine=Medicine);
+//   }
+// medicine:any[]=
+// [
+//   {tablet:"crocine"},
+//   {tablet:"crocine"},
+//   {tablet:"crocine"},
+//   {tablet:"crocine"},
+//   {tablet:"crocine"}
+// ];
+// }
 import { Component, OnInit } from '@angular/core';
-import { ViewappointmentService } from '../../Services/viewappointment.service';
+
 @Component({
   selector: 'app-view-all-prescription',
   templateUrl: './view-all-prescription.component.html',
   styleUrls: ['./view-all-prescription.component.scss']
 })
 export class ViewAllPrescriptionComponent implements OnInit {
-  Medicine: any[]= [];
-  constructor(private  ViewappointmentService:ViewappointmentService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.ViewappointmentService.getmedicine()
-
-    .subscribe(Medicine=>this.medicine=Medicine);
   }
 medicine:any[]=
 [
