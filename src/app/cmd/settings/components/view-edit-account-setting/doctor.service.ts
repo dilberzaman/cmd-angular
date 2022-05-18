3111d5d80bac8ff95bbe7f9462c22851f1d99e27
supriyas.raikar <supriyas.raikar@skillassure.com>
2022-05-18 14:55:34 +0530

    Team 4 bug fixed

--- src/app/cmd/settings/components/view-edit-account-setting/doctor.service.ts
+++ src/app/cmd/settings/components/view-edit-account-setting/doctor.service.ts
@@ -5,16 +5,35 @@ import { HttpClient } from '@angular/common/http'
   providedIn: 'root'
 })
 export class DoctorService {
-  constructor(
-    private http:HttpClient
-  ) { }
+  apiurl: string;
 
-  getDoctorProfile(id:number){
-    return this.http.get("https://localhost:7222/api/Doctor/" + id)
+
+
+  //5.Class Constructor
+
+  //      Inject the services needed for backend services
+
+  //      Inject Router to navigate to other pages
+
+  constructor(private http:HttpClient) {
+
+    //6. Assign backend api to the local variables
+
+    this.apiurl = `https://cmddoctor20220518111744.azurewebsites.net/api/doctor`
+
+   }
+   getdoctordata(){
+
+    return this.http.get<any[]>(`${this.apiurl}`)
+
   }
 
-  updateDoctorProfile(id:number,data:any){
-    return this.http.put("https://localhost:7222/api/Doctor/" + id, data)
-  }
+  // getDoctorProfile(id:number){
+  //   return this.http.get("https://cmddoctor20220512222633.azurewebsites.net/api/Doctor" + id)
+  // }
+
+  // updateDoctorProfile(id:number,data:any){
+  //   return this.http.put("https://localhost:7222/api/Doctor/" + id, data)
+  // }
   
 }
